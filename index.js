6714d33c97e7472a2f3c0b2f35d3f0d0078606a4
Ujjawal Singh <96330274+uksingh19@users.noreply.github.com>
2022-05-28 13:53:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ randomNumber2 = randomNumber2 * 6;
 randomNumber2 = Math.floor(randomNumber2);
 randomNumber2 = randomNumber2 + 1;
 
-document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.querySelectorAll("img")[0].setAttribute("src", "dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src", "dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
